0111d28c0ca36b710e7914703f6b8516d8cb9995
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2024-02-07 00:44:49 +0200

    feb 6 updated

--- src/screens/contact-page/Contact.js
+++ src/screens/contact-page/Contact.js
@@ -1,39 +1,33 @@
-import React, { useState } from "react";
-import emailjs from '@emailjs/browser';
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    surName: "",
-    message: "",
-  });
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-  const emailTemplate = "template_8qhhv5s"
-  const serviceTemplate = "service_rcgg1kt"
 
-const sendEmail = ()=>{
-  emailjs.sendForm(emailTemplate,serviceTemplate,handleInputChange)
-}
+
+  const [state, handleSubmit] = useForm("mpzvpolb");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
 
   return (
     <>
       <p className="lg:text-[35px] lg:text-center pt-[50px] lg:pt-[150px] text-[25px] text-[red] lg:pb-[60px] bg-[#F7F6FB]">
         Contact Me
       </p>
+
       <div
         className="lg:pt-[50px] px-[10px] pt-[50px] flex lg:gap-[200px] pb-[100px] flex-col-reverse lg:flex-row lg:pl-[130px] bg-[#F7F6FB]"
         id="contact"
       >
         <div>
-          <form className="lg:w-[500px]">
+          <form onSubmit={handleSubmit} className="lg:w-[500px]">
             <div className="flex flex-col gap-[25px] w-[40%]">
               <div className="flex flex-col lg:flex-row mt-[20px] gap-[20px] w-[100%]">
                 <span>
                   <input
                     placeholder="First Name"
-                    onChange={handleInputChange}
+                    id="firstName"
+                    type="text"
                     name="firstName"
                     className="rounded-[5px] h-[40px] pl-[10px]"
                   />
@@ -42,7 +36,8 @@ const sendEmail = ()=>{
                   <input
                     placeholder="Surname"
                     name="surName"
-                    onChange={handleInputChange}
+                    type="text"
+                    id="surName"
                     className=" rounded-[5px] h-[40px] pl-[10px]"
                   />
                 </span>
@@ -53,14 +48,16 @@ const sendEmail = ()=>{
                   rows={10}
                   placeholder="message"
                   name="message"
-                  onChange={handleInputChange}
+                  id="message"
+                  type="text"
+                 
                   className="rounded-[5px] w-[80vw] lg:w-[480px] p-[5px]"
                 />
               </div>
               <button
                 type="submit"
                 className="bg-[#343D68] text-white py-[10px] px-[30px] font-[Merienda] text-center justify-center rounded-[5px] flex"
-                onClick={sendEmail}
+                disabled={state.submitting}
               >
                 Send Message
               </button>
